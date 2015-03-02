ee0bc4d458ed9f254f46c0439973bba45d5c3ba3
Scott Crawford <scottcrawford03@gmail.com>
2015-03-01 22:33:13 -0700

    styled index page with image and logo. still need forms

--- spec/integration/guest_view_spec.rb
+++ spec/integration/guest_view_spec.rb
@@ -14,5 +14,9 @@ describe "the guest view", type: :feature do
     it "has the site title" do
       expect(page).to have_content('TrainTracks')
     end
+
+    it "has a log in button for spotify" do
+      expect(page).to have_selector("input[type=submit][value='LOGIN WITH SPOTIFY']")
+    end
   end
 end
