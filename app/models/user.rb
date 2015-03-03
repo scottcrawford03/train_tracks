ac2d4c17375fd551382049d151aeeaec637db649
Scott Crawford <scottcrawford03@gmail.com>
2015-03-03 13:05:54 -0700

    trying to get fonts to work on heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  validates :provider, presence: true
+  validates :uid, presence: true
+  validates :name, presence: true
+  validates :token, presence: true
+
   def self.find_or_create_from_auth(auth)
     user = User.find_or_create_by(provider: auth.provider, uid: auth.uid)
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "the user", type: :feature do
+  before do
+    @user = User.create(name: "Scott Crawford",
+                       image_url: "google.com",
+                       token: "1234",
+                       uid: "abc",
+                       provider: "spotify" )
+  end
+
+  it "has attributes" do
+    expect(@user.name).to eq("Scott Crawford")
+    expect(@user.image_url).to eq("google.com")
+    expect(@user.token).to eq("1234")
+    expect(@user.uid).to eq("abc")
+    expect(@user.provider).to eq("spotify")
+  end
+
+  it "is invalid without a provider" do
+    @user.provider = nil
+    expect(@user).to_not be_valid
+  end
 end
