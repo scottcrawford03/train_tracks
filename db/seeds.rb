b2fe197771f796328b65332efc20d8ddbabda2ac
Scott Crawford <scottcrawford03@gmail.com>
2015-03-05 08:42:31 -0700

    added some seeds to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Track.create(spotify_artist_name: "Taylor Swift", spotify_track_name: "Shake It Off", avg_bpm: "130", length: 300)
+Track.create(spotify_artist_name: "Justin Bieber", spotify_track_name: "Baby", avg_bpm: "127", length: 200)
+Track.create(spotify_artist_name: "Miley Cyrus", spotify_track_name: "The Climb", avg_bpm: "90", length: 200)
+Track.create(spotify_artist_name: "James Blunt", spotify_track_name: "Beautiful", avg_bpm: "50", length: 175)
+Track.create(spotify_artist_name: "Carly Rae Jepsin", spotify_track_name: "Call Me Maybe", avg_bpm: "143", length: 175)
+Track.create(spotify_artist_name: "ABBA", spotify_track_name: "Dancing Queen", avg_bpm: "123", length: 220)
+Track.create(spotify_artist_name: "Lionel Richie", spotify_track_name: "Hello", avg_bpm: "40", length: 220)
+Track.create(spotify_artist_name: "The Fray", spotify_track_name: "How to Save a Life", avg_bpm: "80", length: 190)
+Track.create(spotify_artist_name: "P!NK", spotify_track_name: "Sober", avg_bpm: "122", length: 190)
